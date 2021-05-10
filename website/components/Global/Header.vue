79b1f83d4090c3f1c13861ecfc764c780b831797
Kentleigh English <kentleigh@ikengainnovations.com>
2021-05-10 23:52:02 +0100

    Add more layout components

---
+++ website/components/Global/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div className="container">
+      <GlobalNav />
+    </div>
+    <Masthead v-if="isHome" />
+  </header>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { mapState } from "vuex";
+
+export default Vue.extend({
+  computed: {
+    ...mapState({
+      isHome(): boolean {
+        // eslint-disable-next-line no-console
+        console.log(this.$route);
+        return true;
+      },
+    }),
+  },
+});
+</script>
---
+++ website/components/Global/Masthead.vue
@@ -0,0 +1,34 @@
+import { applyModifiers } from "@src/utilities/utility";
+
+<template>
+  <div class="masthead">
+    <div class="container">
+      <div class="masthead__blocks">
+        <div
+          v-for="(block, i) in blocks"
+          :key="`block_${i}`"
+          apply-modifiers="masthead__block"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    blocks: [
+      ["left", "thick", "very-tall"],
+      ["left", "thick", "tall"],
+      ["left", "very-thin", "short"],
+      ["right", "thin", "very-tall"],
+      ["right", "thick", "tall"],
+      ["right", "very-thin", "short"],
+      ["right", "thick", "very-short"],
+    ],
+  }),
+});
+</script>
+
+export default Masthead;
