d282a49ff86fb5e00ec3c86b20e6ba981227f231
Kentleigh English <kentleigh@ikengainnovations.com>
2021-05-15 08:08:58 +0100

    Move editor config

--- server/socket/index.js
+++ server/socket/index.js
@@ -5,30 +5,34 @@ const debugFunc = require("debug");
 const debug = debugFunc("socket:server");
 
 const hookEvents = (socketIO) => {
-	socketIO.on('connect', (socket) => {
-		const { handshake: { query } } = socket;
-		debug("Remote connection");
+  socketIO.on('connect', (socket) => {
+    const { handshake: { query } } = socket;
+    debug("Remote connection");
 
-		if (query && query.user) {
-			const user = JSON.parse(query.user);
+    if (query && query.user) {
+      const user = JSON.parse(query.user);
 
-			if (user) {
-				debug(`User Connected: ${user.email}`);
-			}
-		}
-	});
+      if (user) {
+        debug(`User Connected: ${user.email}`);
+      }
+    }
+
+    if (query?.type === "relay") {
+
+    }
+  });
 };
 
 module.exports = (server) => {
-	const socketIO = io(server, {
-		path: config.socket.path,
-		cors: {
-			origin: config.cors.origins.split(','),
-			methods: config.cors.methods.split(','),
-		}
-	});
-	
-	hookEvents(socketIO);
-	
-	return socketIO;
+  const socketIO = io(server, {
+    path: config.socket.path,
+    cors: {
+      origin: config.cors.origins.split(','),
+      methods: config.cors.methods.split(','),
+    }
+  });
+
+  hookEvents(socketIO);
+
+  return socketIO;
 };
